b112919041eccefb762f40995202b400ce74f628
kariewas <kari.ewasechko@gmail.com>
2018-12-06 16:11:48 -0800

    Update Event.java

--- Event.java
+++ Event.java
@@ -2,13 +2,15 @@
 // event representation
 class Event implements Comparable {
 
- public Event(int a_type, double a_time) { _type = a_type; time = a_time; }
+ public Event(int a_type, double a_time, int a_seqNum) { _type = a_type; time = a_time; seqNum = a_seqNum; }
   
  public double time;
  private int _type;
+ public int seqNum;
  
  public int get_type() { return _type; }
  public double get_time() { return time; }
+ public int get_seqNum() { return seqNum; }
 
  public Event leftlink, rightlink, uplink;
 
